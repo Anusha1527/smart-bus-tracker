824e85eb4112e771d763f1b166be8afd66b8b00c
Anusha1527 <anushavreddy5@gmail.com>
2025-12-02 18:01:05 +0530

    Obscure Firebase configuration values
    Replaced sensitive Firebase configuration values with placeholders.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_YQ1NvaaLVrwDsphhHeKy_pfWFP0fX0o",
-  authDomain: "smart-bus-tracker-6aea2.firebaseapp.com",
-  projectId: "smart-bus-tracker-6aea2",
-  storageBucket: "smart-bus-tracker-6aea2.firebasestorage.app",
-  messagingSenderId: "169178270498",
-  appId: "1:169178270498:web:53d3fae1c7e9d58d02337b"
+  apiKey: "....",
+  authDomain: ".....",
+  projectId: "......",
+  storageBucket: ".....",
+  messagingSenderId: ".....",
+  appId: "....."
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
